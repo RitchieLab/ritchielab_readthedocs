41a46ac18017d99cec44062c279199c7b74a0273
XueqiongLi <lixueqiong1118@gmail.com>
2024-06-27 14:03:53 -0400

    test

--- docs/extra.js
+++ docs/extra.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    var flyout = document.getElementsByClassName("floating container bottom-right")[0]
-    flyout.removeClass('floating bottom-right')
-    flyout.addClass('wy-nav-side')
+    var flyout = document.querySelectorAll('.floating, .container, .bottom-right')
+    for (let i = 0; i < flyout.length; i++) {
+        flyout.removeClass('floating bottom-right')
+        flyout.addClass('wy-nav-side')
+    }
 });
