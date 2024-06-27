dcce26ee0623db81c20089390dbe7d8ab6da7f24
XueqiongLi <lixueqiong1118@gmail.com>
2024-06-27 14:18:55 -0400

    remove flyout

--- docs/extra.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var flyout = document.querySelectorAll('.floating, .container, .bottom-right');
-    for (let i = 0; i < flyout.length; i++) {
-        flyout.removeClass('floating bottom-right');
-        flyout.addClass('wy-nav-side');
-    }
-});
